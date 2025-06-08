03d9861a40636a1a11a393b445cfc188f4e5be56
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2025-06-09 04:29:41 +0900

    Add migration file for zettels

---
+++ migrations/2025-06-08-192814_create_zettels/up.sql
@@ -0,0 +1,9 @@
+-- Your SQL goes here
+CREATE TABLE zettels (
+    id TEXT NOT NULL PRIMARY KEY,
+    title TEXT NOT NULL,
+    type TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    archived BOOLEAN NOT NULL DEFAULT FALSE,
+);
